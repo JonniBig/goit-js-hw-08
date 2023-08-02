f8543a09ea69cfbe445597e77d0c005dc10c134d
JonniBig <bigun.i88@gmail.com>
2023-08-02 14:34:40 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,14 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
+
+function onPlay({ seconds }) {
+  localStorage.setItem(TIME_KEY, seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(TIME_KEY) || 0);
